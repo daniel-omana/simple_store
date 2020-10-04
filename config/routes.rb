75bb8a7803d38a86379afbb2d1095569cce9fe36
Daniel <danielomana@shaw.ca>
2020-10-04 12:14:38 -0500

    Controller added - Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/action'
+  get 'articles/index'
+  get 'articles/action'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
